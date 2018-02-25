af0c0f44b503f2a735f1f96e72edfcf7478065c4
jayven <kassarar@gmail.com>
2018-02-25 16:55:05 +0800

    添加一个示意图

--- svc/doc.go
+++ svc/doc.go
@@ -1,5 +1,18 @@
-// Package libsvc 提供一个服务框架，该框架的核心类型是“服务” Service ：一组可供调用的方法
-//
-// 服务可以有很多种实现，例如在本进程内有实际实现的服务：NewLocalService，
-// 又如在远程进程中实现的远程服务等，最终使用者面对的都是统一的 Service 接口
+/*
+Package libsvc 提供一个服务框架，主要类型跟函数之间的关系图如下：
+
+	                                                                        +-- InprocClient()
+	                      Make()                                            |
+	     +------------------------------------------------ ServiceClient <--+-- NewRPCClient(RPCClientProtocolFactory, RPCTransportClient)
+	     |                                                     * ^
+	     v      BindInterface()                                * *
+	  Service -------------------> ServiceWithInterface  (req) * * (resp)
+	                                     ^  |                  * *
+	                                     |  |                  * *          +-- InprocServer()
+	                                     |  |                  v *          |
+	  NewLocalService() -----------------+  +------------> ServiceServer <--+-- NewRPCServer(RPCServerProtocolFactory, RPCTransportServer)
+	                                           Register()
+
+核心类型是“服务” Service（以及 ServiceWithInterface）：可对其方法发起调用
+*/
 package libsvc
